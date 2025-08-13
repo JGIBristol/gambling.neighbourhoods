05c30739a73db7f4bac20abf9b0e238c97737e1e
Rita Rasteiro <rita.rasteiro@gmail.com>
2025-08-13 16:49:58 +0100

    poc

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # gambling-neighbourhoods
+
+This repository contains code and data (just gambling data for now, as geographic is too large for this repo) for analyzing gambling premises in neighbourhoods.
+
+## Proof of Concept (PoC)
+The poc.R file is a PoC script that reads in gambling premises data and geographical data, cleans it, and performs HDBSCAN clustering.
+
+Some of the key steps include:
+1. Loading necessary libraries.
+2. Reading in gambling premises and geographical data.
+3. Cleaning the data by merging the two datasets. I created the function `clean_data` that merges the gambling premises data with the geographical data based on postcode.
+4. Creating a spatial points data frame using the `sp::SpatialPointsDataFrame` function. I am using the WGS84 projection.
+5. Generating a distance matrix using the `geosphere::distm` function.
+6. Running HDBSCAN clustering on the distance matrix.
+
+
+## Future Work
+The idea is to create a R package that can be used in this project. The package will start by including functions for cleaning the data, generating distance matrices, and performing clustering, and adding more functionality as information is included.
---
+++ poc.R
@@ -0,0 +1,75 @@
+#   This is a Proof of Concept (PoC) script for clustering gambling premises data.
+#   It reads in gambling premises data and geographical data, cleans it, and performs HDBSCAN clustering.
+# Load necessary libraries
+
+library(dplyr)
+# Load geospatial libraries
+library(sp)
+library(geosphere)
+# Load libraries for clustering
+library(dbscan)
+
+# Load ggplot2 for plotting
+library(ggplot2)
+
+
+# read geo data
+gc <- read.csv("data/premises-licence-register_06082025.csv", header = TRUE)
+
+geo <- read.csv("data/ONSPD_Online_Latest_Centroids_1060347089026503474.csv", header = TRUE)
+
+
+
+#' Function to clean data by merging gc_data with geo_data
+#' @param gc_data Data frame containing gambling premises data
+#' @param geo_data Data frame containing geographical data
+#' @return Merged data frame with cleaned and relevant columns
+#' @examples
+#' clean_data(gc_data, geo_data)
+clean_data <- function(gc_data, geo_data) {
+  # Assuming gc and geo are data frames with appropriate columns
+    #  rename columns for clarity # nolint: indentation_linter.
+    geo_data["Postcode"] <- geo_data$PCD
+    # select relevant columns
+    geo_data <- geo_data[, c("Postcode", "LAT", "LONG", "IMD", "LSOA21")]
+    #  remove spaces between postcode elements
+    geo_data$Postcode <- gsub(" ", "", geo_data$Postcode)
+    gc_data$Postcode <- gsub(" ", "", gc_data$Postcode)
+    # add ID column to gc_data
+    gc_data$ID <- seq_len(nrow(gc_data))
+    #  subset geo data when Postcode matches
+    geo_data <- geo_data[geo_data$Postcode %in% gc_data$Postcode, ]
+    # merge gc_data with geo_data on Postcode
+    merged_data <- merge(gc_data, geo_data, by = "Postcode", all.x = TRUE)  
+    # some postcodes were wrongly formatted. Around 17 rows
+    # Some more cleaning is needed (TODO)
+    #  But for now I willremove the rows with NA in Lat and Long
+    merged_data <- merged_data[which(merged_data$LAT!="NA"),]
+
+    # return the cleaned and merged data
+    return(merged_data)
+}
+
+data <- clean_data(gc,geo)
+
+# 1. Make spatialpointsdataframe
+xy <- sp::SpatialPointsDataFrame(
+  matrix(c(data$LAT, data$LONG), ncol=2), data.frame(ID=seq(1:length(data$LAT))),
+  proj4string=sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+
+
+# 2. Generate a distance matrix.
+mdist <- geosphere::distm(xy)
+
+
+# 3. Run HDBSCAN clustering
+# Using sys.time to measure execution time
+# trying with minPts = 10
+sys.time(res <- dbscan::hdbscan(mdist, minPts = 10))
+plot(res)
+
+# coredist()
+dbscan::hullplot(mdist, res, title = "HDBSCAN Clustering of Gambling Premise (minPts = 10)")
+
+dbscan::clplot(mdist, res, main = "HDBSCAN Clustering of Gambling Premise (minPts = 10)")
+
